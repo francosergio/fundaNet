8b92bbd30e660cdb8dd77f51415f2b40411a776e
FrancoSer_Dev <francosergio_08@hotmail.com>
2022-09-20 16:44:21 -0500

    curso de intro a .net

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Humanizer;
+
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, age please!");
+
+int age= int.Parse(Console.ReadLine());
+Console.WriteLine($"tengo {age.ToWords(new System.Globalization.CultureInfo("es") )}");
